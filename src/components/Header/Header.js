4418d69e3003373cd44bca8f5eb3ff6baa9f0013
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-02 10:13:33 +0600

    tshirt with react router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome To Tshirt Mania !!!</h2>
+            <nav>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
